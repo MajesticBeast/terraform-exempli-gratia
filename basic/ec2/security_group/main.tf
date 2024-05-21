638e333a377efd469fc7d243382b3b3649bc2113
MajesticBeast <majestic@thelostsons.net>
2024-05-20 18:56:25 -0700

    add basic/security_group

---
+++ basic/ec2/security_group/main.tf
@@ -0,0 +1,23 @@
+# Create a security group using the default VPC in us-west-1.
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.50.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_security_group" "this" {
+}
+
+resource "aws_vpc_security_group_ingress_rule" "this" {
+  security_group_id = aws_security_group.this.id
+  ip_protocol       = "-1"
+  cidr_ipv4 = "10.0.0.0/16"
+}
