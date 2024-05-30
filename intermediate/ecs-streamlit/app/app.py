cc03dae4f96e071c0763d16c7a15a01565c0f16a
MajesticBeast <majestic@thelostsons.net>
2024-05-29 20:01:58 -0700

    update app.py to be siiick

--- intermediate/ecs-streamlit/app/app.py
+++ intermediate/ecs-streamlit/app/app.py
@@ -1,2 +1,9 @@
 import streamlit as st
-st.title("Hello, Streamlit!")
+import pandas as pd
+import numpy as np
+
+st.header("Sweet Streamlit App")
+st.write("Dope chart showing mad skillz")
+chart_data = pd.DataFrame(np.abs(np.random.randn(20, 3)), columns=["What you know", "What you think you know", "What you actually know"])
+
+st.line_chart(chart_data)
